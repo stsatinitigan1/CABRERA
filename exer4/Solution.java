01f7c7069b915ae44953afcced3f38cc6f5eef63
Reuben Joseph B. Cabrera <rjbc159@gmail.com>
2025-02-27 19:47:45 +0800

    pushed base code for exer 4

---
+++ exer4/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        // Test case
+
+        System.out.println(solution(5,5) == 25) // true
+        System.out.println(solution(10,5) == 50) // true
+        System.out.println(solution(15,12) == 180) // true
+    }
+  
+    // Problem
+    // write a recursive function to multiply
+    // two positive integers without using * or /
+    // you can se addition/subtraction and bit shifting 
+    // but minimize the number of operations
+    public static int solution(int a, int b) {
+      
+    }
+  }
